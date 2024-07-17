4b01551b3130c24eb63914af8b90d897c0875435
vvu060 <vvu060@gmail.com>
2024-07-17 17:06:46 +0530

    modify home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from 'next/image';
+import { ArrowLeftCircle } from 'lucide-react';
 
 export default function Home() {
-  return <main className=''>Hi</main>;
+  return (
+    <main className='flex space-x-2 items-center animate-pulse'>
+      <ArrowLeftCircle className='w-12 h-12' />
+      <h1 className='font-bold'>Get started with creating a New Document</h1>
+    </main>
+  );
 }
